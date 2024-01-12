80d463573533beb734d9adeda141e5205a4cab49
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2024-01-12 18:44:16 +0500

    feat: Changed loader implementation to show on top of everything

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", from: "5.0.0"),
-         .package(url: "https://github.com/smilesiosteam/SmilesFontsManager.git", branch: "main")
+         .package(url: "https://github.com/smilesiosteam/SmilesFontsManager.git", branch: "main"),
+         .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +26,8 @@ let package = Package(
             name: "SmilesLoader",
             dependencies: [
                 .product(name: "NVActivityIndicatorView", package: "NVActivityIndicatorView"),
-                .product(name: "SmilesFontsManager", package: "SmilesFontsManager")
+                .product(name: "SmilesFontsManager", package: "SmilesFontsManager"),
+                .product(name: "SmilesUtilities", package: "SmilesUtilities")
             ])
     ]
 )
--- Sources/SmilesLoader/SmilesLoader.swift
+++ Sources/SmilesLoader/SmilesLoader.swift
@@ -1,39 +1,17 @@
+
 import UIKit
-import NVActivityIndicatorView
-import SmilesFontsManager
+import SmilesUtilities
 
 @objc public class SmilesLoader: NSObject {
     
-    private static var keyWindow: UIWindow? {
-        return UIApplication.shared.connectedScenes
-            .compactMap({$0 as? UIWindowScene})
-            .first?.windows
-            .filter({$0.isKeyWindow}).first
-    }
-    
     @objc public static func show(with message: String? = nil, isClearBackground: Bool = false) {
         
-        if let window = keyWindow {
-            
-            guard !window.subviews.contains(where: { $0 is BlockingActivityIndicator }) else {
-                return
-            }
-            
-            let activityIndicator = BlockingActivityIndicator()
-            activityIndicator.setupMessage(message: message)
-            activityIndicator.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            activityIndicator.frame = window.bounds
-            if isClearBackground {
-                activityIndicator.backgroundColor = .clear
-            }
-            if let _ = window.rootViewController as? UITabBarController {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                    window.addSubview(activityIndicator)
-                }
+        if let topVC = UIApplication.getTopViewController(), !topVC.children.contains(where: {$0 is LoadingViewController}) {
+            let vc = LoadingViewController(message: message, isClearBackground: isClearBackground)
+            if let tabBar = topVC.tabBarController {
+                tabBar.addChild(asChildViewController: vc, diseredView: tabBar.view)
             } else {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                    window.addSubview(activityIndicator)
-                }
+                topVC.addChild(asChildViewController: vc, diseredView: topVC.view)
             }
         }
         
@@ -58,18 +36,14 @@ import SmilesFontsManager
     
     @objc public static func dismiss() {
         
-        if let window = keyWindow {
-            if let _ = window.rootViewController as? UITabBarController {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                    window.subviews.filter { $0 is BlockingActivityIndicator }.forEach { view in
-                        view.removeFromSuperview()
-                    }
+        if let topVC = UIApplication.getTopViewController() {
+            if let tabBar = topVC.tabBarController {
+                if let loadingVC = tabBar.children.first(where: {$0 is LoadingViewController}) {
+                    topVC.removeChild(asChildViewController: loadingVC)
                 }
             } else {
-                window.subviews.filter { $0 is BlockingActivityIndicator }.forEach { view in
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        view.removeFromSuperview()
-                    }
+                if let loadingVC = topVC.children.first(where: {$0 is LoadingViewController}) {
+                    topVC.removeChild(asChildViewController: loadingVC)
                 }
             }
         }
@@ -82,74 +56,6 @@ import SmilesFontsManager
             obj.removeFromSuperview()
         }
         
-    }
-    
-}
-
-final class BlockingActivityIndicator: UIView {
-    
-    private let loaderColor = UIColor(red: 135.0 / 255.0, green: 84.0 / 255.0, blue: 161.0 / 255.0, alpha: 1.0)
-    private let containerView: UIStackView = {
-        let view = UIStackView()
-        view.axis = .vertical
-        view.spacing = 12
-        view.alignment = .fill
-        view.distribution = .fill
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    private let messageLabel: UILabel = {
-        let view = UILabel()
-        view.fontTextStyle = .smilesHeadline5
-        view.textAlignment = .center
-        view.numberOfLines = 2
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    private let activityIndicator: NVActivityIndicatorView
-    
-    override init(frame: CGRect) {
-        self.activityIndicator = NVActivityIndicatorView(
-            frame: CGRect(
-                origin: .zero,
-                size: CGSize(width: 60, height: 60)
-            )
-        )
-        activityIndicator.type = .ballClipRotate
-        activityIndicator.color = loaderColor
-        activityIndicator.startAnimating()
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        messageLabel.textColor = loaderColor
-        super.init(frame: frame)
-        backgroundColor = UIColor.white.withAlphaComponent(0.75)
-        setupViews()
-        
-    }
-    
-    private func setupViews() {
-        
-        containerView.addArrangedSubview(activityIndicator)
-        containerView.addArrangedSubview(messageLabel)
-        addSubview(containerView)
-        
-        NSLayoutConstraint.activate([
-            containerView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
-            containerView.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor),
-            containerView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 1.2)
-        ])
-        
-    }
-    
-    func setupMessage(message: String?) {
-//        if let message {
-//            messageLabel.text = message
-//        } else {
-            messageLabel.isHidden = true
-//        }
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
 }
